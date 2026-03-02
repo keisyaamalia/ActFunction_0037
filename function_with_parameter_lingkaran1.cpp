ef0e5407451bc99c2cef162a5fd99cb495e46478
keisyaamalia <kkaysaamalia@gmail.com>
2026-03-02 12:03:40 +0700

    function with parameter lingkaran 2

--- function_with_parameter_lingkaran1.cpp
+++ function_with_parameter_lingkaran1.cpp
@@ -8,3 +8,12 @@ void input()
     cout << "Masukkan jari jari :";
     cin >> r;
 }
+float luaslingkaran(int r)
+{
+    return 3.14159 * r * r;
+}
+
+void output()
+{
+    cout << "Luasnya : " << luaslingkaran(r);
+}
